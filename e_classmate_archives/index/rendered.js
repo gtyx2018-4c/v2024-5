cdb787de81e00655b7b0c58452c513cb69bf3868
sungbly1tsstt <onily.code_sungbly_tsstt@outlook.com>
2024-06-24 11:59:43 +0800

    remake redender archives

--- e_classmate_archives/index/rendered.js
+++ e_classmate_archives/index/rendered.js
@@ -1,16 +1,18 @@
 //渲染-加载列表
-var tmp_card;
+//var tmp_card;
 var Maindiv;
-var Card_in_road;
+//var Card_in_road;
 Maindiv = document.getElementById("zj-main-table");
 function renCard(time,list){
+    /*
     var runed = 0;
     var i = 0;
     var limitcard = time;
     var oruned;
     console.log("运行"+time+"次");
     console.log("传入数据:");
-    console.log(list);
+    console.log(list);*/
+    /*
     while(runed<time){
         runed = ++runed
         oruned = runed;
@@ -52,7 +54,48 @@ function renCard(time,list){
         document.getElementById(list["Number"+i]).innerHTML="<br><br><h1 style='color: black;text-align: center;width: 100%;'>"+list["Name"+i]+"<br><h2 style='color: green;text-align: center;width: 100%;'>"+list["Number"+i]+"号"+"</h2><p style='color: gray;text-align: center;width: 100%;'>点击查看详情</p>";
         //Maindiv.innerHTML=Maindiv.innerHTML+"&#62;/td&#60;";
         //Maindiv.innerHTML=Maindiv.innerHTML+"&#62;/tr&#60;";
-        */
+        *-/
+    } 妈的傻逼重写*/
+    /*参数:
+    time:总项目数
+    list:同学录表
+    */
+    var runed = 0;//已运行几次(列)
+    var all_runed = 0;//总运行次数
+    var card_in_a_road = 0;//该列存在多少个表格
+    var finish = false;//true跳出while
+    var NowList;//现工作行
+    //Maindiv是渲染目标
+    console.log('list',list);
+    while(finish == false){//运行次数
+        runed = runed+1
+        if(runed == 1){//直接写入会似
+            Maindiv.innerHTML="<tr id='"+runed+"tr'>";//创建一列
+        }else{
+            Maindiv.innerHTML=Maindiv.innerHTML + "<tr id='"+runed+"tr'>";//原有基础上再建一列
+        }
+        NowList = document.getElementById(runed+"tr");//获取当前行
+        while(card_in_a_road<5){//循环到5张卡片填满
+            all_runed = all_runed+1//有没有都得加
+            if(time < all_runed){//是不是搞完了
+                card_in_a_road = 5;
+                finish = true;//结束循环
+            }
+            if(typeof list["Name"+all_runed] == "undefined"){//是不是没有
+                //不管他
+            }else{
+                card_in_a_road = card_in_a_road + 1;//加卡片数量
+                if(card_in_a_road == 1){
+                    document.getElementById(runed+"tr").innerHTML="<td id='"+all_runed+"td'>";//开始创建卡片容器
+                    document.getElementById(all_runed+"td").innerHTML = "<div class='zj-256' onclick='location.href = \"archives.html?flag="+ all_runed +"\";'><br><p style='text-align: center;width: 100%;'><h1>"+list["Name"+all_runed]+"</h1></p><br><br><h1><span style='color:green;'>"+list["Number"+all_runed]+"号"+"</span></h1><p style='width: 100%;text-align: center;color: grey;'>点击查看详情</p></div>";//创建卡片
+                }else{
+                    document.getElementById(runed+"tr").innerHTML=document.getElementById(runed+"tr").innerHTML+"<td id='"+all_runed+"td'>";//开始增加卡片容器
+                    document.getElementById(all_runed+"td").innerHTML = document.getElementById(all_runed+"td").innerHTML+"<div class='zj-256' onclick='location.href = \"archives.html?flag="+ all_runed +"\";'><br><p style='text-align: center;width: 100%;'><h1>"+list["Name"+all_runed]+"</h1></p><br><br><h1><span style='color:green;'>"+list["Number"+all_runed]+"号"+"</span></h1><p style='width: 100%;text-align: center;color: grey;'>点击查看详情</p></div>";//增加卡片
+                }
+                console.log('card_in_a_road',card_in_a_road);
+            }
+        }//五张以后
+        card_in_a_road = 0;
     }
     document.getElementById("loadText").innerHTML="<span style='color:green;'>加载完成</span>";
 }
